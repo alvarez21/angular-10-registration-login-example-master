9e4611bca806ce7dbedc5aa590d6504b92860d4f
alvarez21 <lhecdeekhan01@gmail.com>
2021-04-19 23:40:25 +0800

    description

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,9 +29,7 @@ import { HomeComponent } from './home';
         { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
 
         // provider used to create fake backend
-        
-         
-        //fakeBackendProvider
+    
     ],
     bootstrap: [AppComponent]
 })
